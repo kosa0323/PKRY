ec609660522047e3c2a970db94006a413a2bf8c9
kosa0323 <mateusz9423@gmail.com>
2016-01-21 15:20:36 +0100

    PKRY

---
+++ Klient/Kupujacy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klient
+{
+    class Kupujacy
+    {
+        public string NazwaMarkiKarty = "MasterCard";
+        public string NumerKarty = "1234567890";
+       
+        public Kupujacy()
+        {
+            NazwaMarkiKarty = "MasterCard";
+            NumerKarty = "1234567890";
+        }
+    }
+
+}
---
+++ Klient/Protokol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klient
+{
+    class Protokol
+    {
+        public const string PURCHASE_INIT = "#Inicjuj_Zakupy";
+        public const string PURCHASE_INIT_RES = "#Inicjuj_Zakupy_Odpowiedz";
+        public const string PURCHASE_REQ = "#Zapytanie_Zakupy";
+        public const string PURCHASE_REQ_RES = "#Zapytanie_Zakupy_Odpowiedz";
+        public const string BLAD = "#Błąd_w_Komunikacji";
+        public const string AUTHORIZATION_REQ = "#Zapytanie_o_Autoryzację";
+        public const string AUTHORIZATION_RES = "#Odpowiedz_o_Autoryzację";
+        public const string PAYMENT_REQ = "#Zapytanie_o_Platnosc";
+        public const string PAYMENT_RES = "#Odpowiedz_o_Platnosc";
+    }
+}
